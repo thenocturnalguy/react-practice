dc896ca90cc6634ff8e545f1b26db801a00990f5
theNocturnalGuy <rahulgupta0097@gmail.com>
2020-05-30 23:05:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+	render() {
+		return (
+			<Fragment>
+				<h1>Hello World! This is Rahul.</h1>
+				<p>I'm an Amateur React Developer.</p>
+			</Fragment>
+		);
+	}
+}
+
+export default App;
